ea96d1082b7a2c8b3b1acf00d7d8e5173e5d5e44
lucsbandeira06 <lucasbandeira06@gmail.com>
2022-09-22 17:18:58 +0100

    First commit

---
+++ app/src/main/java/ie/dorset/lucasbandeira23884/dorset_mobile_app_2/MainFragment.kt
@@ -0,0 +1,36 @@
+package ie.dorset.lucasbandeira23884.dorset_mobile_app_2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ie.dorset.lucasbandeira23884.dorset_mobile_app_2.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.takeIf { it.containsKey("NEW_TEXT") }.apply {
+            val newText = arguments?.getString("NEW_TEXT")
+            binding.helloBlankFragment.text = newText
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
