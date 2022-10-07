c544dba1d137b7d6302e1cdc867ed1a4163a5f7c
lucsbandeira06 <lucasbandeira06@gmail.com>
2022-10-07 14:31:30 +0100

    Last implementation

--- app/src/main/java/ie/dorset/lucasbandeira23884/dorset_mobile_app_2/DatabaseActivity.kt
+++ app/src/main/java/ie/dorset/lucasbandeira23884/dorset_mobile_app_2/DatabaseActivity.kt
@@ -3,9 +3,9 @@ package ie.dorset.lucasbandeira23884.dorset_mobile_app_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainFragment : AppCompatActivity() {
+class DatabaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_fragment)
+        setContentView(R.layout.activity_database)
     }
 }
